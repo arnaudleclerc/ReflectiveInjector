39b83ef76faa99a7522dd7b58b7edc7a2c2382c7
arnaudleclerc <arnaud.leclerc@tekaris.com>
2018-06-14 07:41:44 +0200

    Throw exception if not start name is given

--- src/ReflectiveInjector.Extensions/ReflectiveInjectorMiddleware.cs
+++ src/ReflectiveInjector.Extensions/ReflectiveInjectorMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using ReflectiveInjector.Core;
@@ -9,25 +10,29 @@ namespace ReflectiveInjector.Extensions
     public static class ReflectiveInjectorMiddleware
     {
         /// <summary>
-        /// Registers the services using reflection
+        /// Registers services using reflection
         /// </summary>
         /// <param name="services">Service collection used for the injection</param>
         /// <param name="libraryStartName">Start name of the libraries on which the injector will look for services to register</param>
         public static IServiceCollection AddReflectorDependencies(this IServiceCollection services, string libraryStartName)
         {
+            if (string.IsNullOrWhiteSpace(libraryStartName))
+            {
+                throw new ArgumentException("A start name for the libraries to look into should be provided");
+            }
+
             var dependencyContextService = new DependencyContextService();
 
             foreach (var library in dependencyContextService.RuntimeLibraries
                 .Where((lib) => lib.Name.ToLower().StartsWith(libraryStartName.ToLower())))
             {
-                dependencyContextService.GetAssemblyTypes(library)
-                    .Where(t => t.GetTypeInfo().IsClass && !t.GetTypeInfo().IsAbstract && typeof(IRegistry).GetTypeInfo().IsAssignableFrom(t))
-                    .ToList()
-                    .ForEach((t) =>
-                    {
-                        var registry = (IRegistry)dependencyContextService.CreateInstance(t);
-                        registry.RegisterServices(services);
-                    });
+                foreach (var type in dependencyContextService.GetAssemblyTypes(library)
+                    .Where(t => t.GetTypeInfo().IsClass && !t.GetTypeInfo().IsAbstract &&
+                                typeof(IRegistry).GetTypeInfo().IsAssignableFrom(t)))
+                {
+                    var registry = (IRegistry)dependencyContextService.CreateInstance(type);
+                    registry.RegisterServices(services);
+                }
             }
 
             return services;
